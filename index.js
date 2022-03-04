2dd6d326e544da9b76d66a08d48aa782e7deee29
Nilton <niltonroese@gmail.com>
2022-03-04 21:57:49 +1000

    Done with assingment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+const drivers = ["Sally", "Bob", "Freddy", "Claudia"];
+
+const returnFirstTwoDrivers = (drivers) => {
+    return drivers.slice(0,2);
+}
+
+const returnLastTwoDrivers = (drivers) => {
+    return drivers.slice(2,4);
+}
+
+const selectingDrivers =  [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = (a) => {
+    return (a) => a**2
+}
+
+const fareDoubler = (a) => {
+return a*2
+}
+
+const fareTripler = (a) => {
+    return a*3
+    }
+
+const selectDifferentDrivers = (arr, fn) => {
+    return fn(arr)
+}
