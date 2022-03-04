f8200f44975afab138bbdc8b9a982305e617383c
Nilton <niltonroese@gmail.com>
2022-03-04 21:26:15 +1000

    commit tree b52c847af8659e68534ecc421110e7e542510bb0

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solution in this file!
+const drivers = ["Sally", "Bob", "Freddy", "Claudia"];
+const returnFirstTwoDrivers = (drivers) => {
+return drivers.slice(0,2);
+}
+
+const returnLastTwoDrivers = (drivers) => {
+    return drivers.slice(2,4);
+}
+
